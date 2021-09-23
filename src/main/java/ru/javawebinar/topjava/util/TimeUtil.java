f27b1253a12421e911efa182e1ccb2ed615f031f
dervishsy <s-y@yandex.ru>
2021-09-23 14:30:22 +0300

    HW0 Циклы и Стримы

--- src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/TimeUtil.java
@@ -1,9 +1,20 @@
 package ru.javawebinar.topjava.util;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class TimeUtil {
     public static boolean isBetweenHalfOpen(LocalTime lt, LocalTime startTime, LocalTime endTime) {
         return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) < 0;
+    }
+
+    public static boolean isLocalDateTimeInLimits(LocalDateTime dateTime, LocalTime startTime, LocalTime endTime) {
+        LocalTime localTime = dateTime.toLocalTime();
+        return !(localTime.isBefore(startTime) || localTime.isAfter(endTime));
+    }
+
+    public static LocalDate getDate(LocalDateTime ldt) {
+        return ldt.toLocalDate();
     }
 }
--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,11 +3,12 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExcess;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class UserMealsUtil {
     public static void main(String[] args) {
@@ -19,21 +20,46 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 10, 0), "Завтрак", 1000),
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 13, 0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2020, Month.JANUARY, 31, 20, 0), "Ужин", 410)
+
         );
 
-        List<UserMealWithExcess> mealsTo = filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
-        mealsTo.forEach(System.out::println);
+        System.out.println(filteredByCycles(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
 
-//        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
+        System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
     }
 
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+
         // TODO return filtered list with excess. Implement by cycles
-        return null;
+        HashMap<LocalDate, Integer> ldhm = new HashMap<>();
+        List<UserMealWithExcess> result = new LinkedList<>();
+
+        for (UserMeal meal : meals) {
+            LocalDate ld = meal.getDateTime().toLocalDate();
+            Integer value = ldhm.getOrDefault(ld,0);
+            ldhm.put(ld,value+meal.getCalories());
+        }
+
+        for (UserMeal meal : meals) {
+            if (!TimeUtil.isLocalDateTimeInLimits(meal.getDateTime(), startTime, endTime)) continue;
+            boolean isExcess = (caloriesPerDay < ldhm.getOrDefault(meal.getDateTime().toLocalDate(),0));
+            result.add(new UserMealWithExcess(meal.getDateTime(), meal.getDescription(), meal.getCalories(), isExcess));
+        }
+        return result;
     }
 
     public static List<UserMealWithExcess> filteredByStreams(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         // TODO Implement by streams
-        return null;
+        Map<LocalDate, Integer> ldhm  = meals.stream()
+                .collect(Collectors.toMap(x-> TimeUtil.getDate(x.getDateTime()), x-> x.getCalories(),(o, n)->o+n));
+
+        return meals.stream()
+                .filter(x -> TimeUtil.isLocalDateTimeInLimits(x.getDateTime(), startTime, endTime))
+                .map(meal -> new UserMealWithExcess(meal.getDateTime(),
+                        meal.getDescription(),
+                        meal.getCalories(),
+                        (ldhm.getOrDefault(TimeUtil.getDate(meal.getDateTime()),0)>caloriesPerDay)))
+                .collect(Collectors.toList());
     }
+
 }
