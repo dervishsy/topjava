d1fd86ceac78974532ea4be1a18412984b258c0e
dervishsy <s-y@yandex.ru>
2021-11-25 08:16:31 +0300

    Lesson 7_0.Fix

---
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -0,0 +1,105 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.javawebinar.topjava.model.Meal;
+import ru.javawebinar.topjava.service.MealService;
+import ru.javawebinar.topjava.util.MealsUtil;
+import ru.javawebinar.topjava.web.SecurityUtil;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.temporal.ChronoUnit;
+import java.util.List;
+import java.util.Objects;
+
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalDate;
+import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
+import static ru.javawebinar.topjava.util.ValidationUtil.assureIdConsistent;
+import static ru.javawebinar.topjava.util.ValidationUtil.checkNew;
+
+@Controller
+@RequestMapping("meals")
+public class JspMealController {
+
+    @Autowired
+    private MealService service;
+
+    @GetMapping
+    public String getAll(Model model) {
+        int userId = SecurityUtil.authUserId();
+        model.addAttribute("meals", MealsUtil.getTos(service.getAll(userId), SecurityUtil.authUserCaloriesPerDay()));
+        return "meals";
+    }
+
+    @GetMapping("filter")
+    public String filter(Model model, HttpServletRequest request) {
+        int userId = SecurityUtil.authUserId();
+
+        LocalDate startDate = parseLocalDate(request.getParameter("startDate"));
+        LocalDate endDate = parseLocalDate(request.getParameter("endDate"));
+        LocalTime startTime = parseLocalTime(request.getParameter("startTime"));
+        LocalTime endTime = parseLocalTime(request.getParameter("endTime"));
+
+        List<Meal> mealsDateFiltered = service.getBetweenInclusive(startDate, endDate, userId);
+
+        model.addAttribute("meals", MealsUtil.getFilteredTos(mealsDateFiltered, SecurityUtil.authUserCaloriesPerDay(), startTime, endTime));
+        return "meals";
+    }
+
+    @GetMapping("delete")
+    public String delete(HttpServletRequest request) {
+        int userId = SecurityUtil.authUserId();
+        int id = getId(request);
+        service.delete(id, userId);
+        return "redirect:/meals";
+    }
+
+    @GetMapping("update")
+    public String update(Model model, HttpServletRequest request) {
+        int userId = SecurityUtil.authUserId();
+        int id = getId(request);
+        Meal meal = service.get(id, userId);
+        model.addAttribute("meal", meal);
+        return "mealForm";
+    }
+
+    @GetMapping("create")
+    public String create(Model model) {
+        Meal meal = new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000);
+
+        model.addAttribute("meal", meal);
+        return "mealForm";
+    }
+
+    @PostMapping()
+    protected String doCreateUpdate(HttpServletRequest request) throws IOException {
+        Meal meal = new Meal(
+                LocalDateTime.parse(request.getParameter("dateTime")),
+                request.getParameter("description"),
+                Integer.parseInt(request.getParameter("calories")));
+
+        int userId = SecurityUtil.authUserId();
+        if (StringUtils.hasLength(request.getParameter("id"))) {
+            assureIdConsistent(meal, getId(request));
+            service.update(meal, userId);
+        } else {
+            checkNew(meal);
+            service.create(meal, userId);
+        }
+        return "redirect:meals";
+    }
+
+    private int getId(HttpServletRequest request) {
+        String paramId = Objects.requireNonNull(request.getParameter("id"));
+        return Integer.parseInt(paramId);
+    }
+}
